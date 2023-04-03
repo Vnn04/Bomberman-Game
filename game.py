44527455b7a6442304ad96ede49ec13a17f26632
Your name <nguyenvanthan3001@gmail.com>
2023-04-04 05:40:28 +0700

    commit message

--- game.py
+++ game.py
@@ -1,82 +1,121 @@
 import pygame
-from pygame.locals import*
-from pynput import keyboard
-import time
+from pygame.locals import *
+import os
 
 pygame.init()
 
-font_small = pygame.font.SysFont('sans', 10)
+# Constants
+WIDTH = 900
+HEIGHT = 600
+PLAYER_SPEED = 51
 
-width = 600
-length = 900
+# game space
+LEFT = 45
+RIGHT = 800
+UP = 60
+DOWN = 500
 
+# Basic color
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255,0,0)
 BLUE = (0,0,255)
 
-player_x = 830
-player_y = 36
-player_width = 50
-player_height = 50
-player = [player_x, player_y] # tọa độ người chơi
+# Load images
+background_image = pygame.image.load(r"C:\Users\nguye\Documents\Bomberman\Choi\Bomberman\background.png")
+background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
+player_image = pygame.image.load(r"C:\Users\nguye\Documents\Bomberman\Choi\Bomberman\player.png")
+player_image = pygame.transform.scale(player_image, (50, 55))
+bomb_image = pygame.image.load(r"C:\Users\nguye\Documents\Bomberman\Choi\Bomberman\bomb.png")
+bomb_image = pygame.transform.scale(bomb_image, (45, 45))
 
-state_left = 45
-state_right = 825
-state_up = 65
-state_down = 500
+# Initialize font
+font_small = pygame.font.SysFont('sans', 10)
 
-screen = pygame.display.set_caption("Bomberman")
-screen = pygame.display.set_mode((length, width))
+# Define Player class
+class Player:
+    def __init__(self, x, y, image):
+        self.x = x
+        self.y = y
+        self.image = image
+
+    def move_up(self):
+        if self.y > UP and (self.x - 17) / PLAYER_SPEED % 2 == 0:
+            self.y -= PLAYER_SPEED
+
+    def move_down(self):
+        if self.y < DOWN and (self.x - 17) / PLAYER_SPEED % 2 == 0:
+            self.y += PLAYER_SPEED
+
+    def move_left(self):
+        if self.x > LEFT and (self.y - 35) / PLAYER_SPEED % 2 == 0:
+            self.x -= PLAYER_SPEED
+
+    def move_right(self) :
+        if self.x < RIGHT and (self.y - 35) / PLAYER_SPEED % 2 == 0:
+            self.x += PLAYER_SPEED
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+
+class Bomb:
+    def __init__(self, x, y, image):
+        self.x = x
+        self.y = y
+        self.image = image
+        self.explode_time = pygame.time.get_ticks() + 3000  # set 3 seconds timer
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
 
 
-background_image = pygame.image.load(r"C:\Users\nguye\Documents\Code Python\background.png").convert()
-background_image = pygame.transform.scale(background_image, (length, width))
+# Initialize player object
+player = Player(17, 35, player_image)
 
-player_image = pygame.image.load(r"C:\Users\nguye\Documents\Code Python\player.png").convert()
-player_image = pygame.transform.scale(player_image, (50, 50))
+# Initialize bomb object
+bomb = None
 
-player_speed = 51
+# Initialize screen
+pygame.display.set_caption("Bomberman")
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+# Main loop
 running = True
+
 while running:
-
-
+    # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-        if event.type == pygame.KEYDOWN:
-            if (event.key == pygame.K_UP):
-                if player[1] > state_up:
-                    player[1] -= player_speed
-                    print("up")
-
-            elif (event.key == pygame.K_DOWN):
-                    if player[1] < state_down:
-                        player[1] = player[1] + player_speed
-                        print("down")
-
-            elif (event.key == pygame.K_LEFT):
-                if player[0] > state_left:
-                    player[0] = player[0] - player_speed
-                    print("left")
-
-            elif (event.key == pygame.K_RIGHT):
-                if player[0] < state_right:
-                    player[0] = player[0] + player_speed
-                    print("right")
+            
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player.move_up()
+            elif event.key == pygame.K_DOWN:
+                player.move_down()
+            elif event.key == pygame.K_LEFT:
+                player.move_left()
+            elif event.key == pygame.K_RIGHT:
+                player.move_right()
+            elif event.key == pygame.K_SPACE and not bomb:  # create bomb when space key is pressed
+                bomb = Bomb(player.x, player.y, bomb_image)
 
 
+    # Draw images and text
     screen.blit(background_image, (0, 0))
-    player_rect = player_image.get_rect() # lấy kích thước của ảnh người chơi
-    player_rect.center = (player_x, player_y) # đặt tâm của ảnh người chơi ở vị trí (player_x, player_y)
-    screen.blit(player_image, player)
+    if bomb:
+        bomb.draw(screen)
+        if pygame.time.get_ticks() > bomb.explode_time:  # explode bomb after 5 seconds
+            bomb = None
+    player.draw(screen)
+
     mouse_x, mouse_y = pygame.mouse.get_pos()
     text_mouse = font_small.render("(" + str(mouse_x) + "," + str(mouse_y) + ")", True, BLACK)
     screen.blit(text_mouse, (mouse_x + 10, mouse_y))
 
+    # Update screen
     pygame.display.flip()
 
+# Quit game
 pygame.quit()
